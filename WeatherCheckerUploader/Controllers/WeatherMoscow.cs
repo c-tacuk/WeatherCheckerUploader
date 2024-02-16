6ca88f7807795c9b6f7b599d871f76fdfedb57b5
Стас Сухомлинов <DlyaSteam1997@gmail.com>
2024-02-16 11:42:48 +0300

    Убрал лишние методы и using

--- WeatherCheckerUploader/Controllers/WeatherMoscow.cs
+++ WeatherCheckerUploader/Controllers/WeatherMoscow.cs
@@ -1,83 +1,12 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace WeatherCheckerUploader.Controllers
 {
     public class WeatherMoscow : Controller
     {
-        // GET: WeatherMoscow
-        public ActionResult Index()
+        public IActionResult Index()
         {
             return View();
-        }
-
-        // GET: WeatherMoscow/Details/5
-        public ActionResult Details(int id)
-        {
-            return View();
-        }
-
-        // GET: WeatherMoscow/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: WeatherMoscow/Create
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: WeatherMoscow/Edit/5
-        public ActionResult Edit(int id)
-        {
-            return View();
-        }
-
-        // POST: WeatherMoscow/Edit/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: WeatherMoscow/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
-
-        // POST: WeatherMoscow/Delete/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
         }
     }
 }
