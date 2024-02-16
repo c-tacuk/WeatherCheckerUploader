49b349871cf41dc919f87e03d02f03c294ed25cd
Стас Сухомлинов <DlyaSteam1997@gmail.com>
2024-02-16 16:14:16 +0300

    Создал отдельный проект для базы данных, установил нугет пакеты для работы с entityframework

---
+++ WeatherCheckerUploader.Db/Models/WeatherArchiveModel.cs
@@ -0,0 +1,23 @@
+﻿namespace WeatherCheckerUploader.Db.Models
+{
+    public class WeatherArchiveModel
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Header { get; set; }
+        public string Description { get; set; }
+        public List<string> ColumnNames { get; set; }
+        public List<string> Dates { get; set; }
+        public List<string> Times { get; set; }
+        public List<string> Temperatures { get; set; }
+        public List<string> RelativeHumidities { get; set; }
+        public List<string> TDs { get; set; }
+        public List<string> AtmosphericPressures { get; set; }
+        public List<string> WindDirections { get; set; }
+        public List<string> WindSpeeds { get; set; }
+        public List<string> Cloudinesses { get; set; }
+        public List<string> Hs { get; set; }
+        public List<string> VVs { get; set; }
+        public List<string> WeatherPhenomenas { get; set; }
+    }
+}
