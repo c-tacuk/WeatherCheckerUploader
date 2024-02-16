59f3e970ba8e70839687d628e78765d4ff629346
Стас Сухомлинов <DlyaSteam1997@gmail.com>
2024-02-16 11:39:15 +0300

    Создрал отдельный класс для работы с exel-файлам, добавил несколько методов для работы с exel-файлом

--- WeatherCheckerUploader/Controllers/MainPageController.cs
+++ WeatherCheckerUploader/Controllers/MainPageController.cs
@@ -1,12 +1,16 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using NPOI.SS.UserModel;
+using NPOI.XSSF.UserModel;
 using System.Diagnostics;
 using WeatherCheckerUploader.Models;
+using WeatherCheckerUploader.WorkWithExel;
 
 namespace WeatherCheckerUploader.Controllers
 {
     public class MainPageController : Controller
     {
         private readonly ILogger<MainPageController> _logger;
+        private readonly ExelMethods exelMethods = new ExelMethods();
 
         public MainPageController(ILogger<MainPageController> logger)
         {
@@ -15,6 +19,17 @@ namespace WeatherCheckerUploader.Controllers
 
         public IActionResult Index()
         {
+            //var elems = exelMethods.GetRowData(3);
+            //Console.WriteLine(exelMethods.GetArchiveHeader());
+            //foreach (string el in elems)
+            //{
+            //    Console.WriteLine(el);
+            //}
+            var elems = exelMethods.GetColumnsNames();
+            foreach (string el in elems)
+            {
+                Console.WriteLine(el);
+            }
             return View();
         }
     }
---
+++ WeatherCheckerUploader/WorkWithExel/ExelMethods.cs
@@ -0,0 +1,58 @@
+﻿using NPOI.SS.UserModel;
+using NPOI.XSSF.UserModel;
+
+namespace WeatherCheckerUploader.WorkWithExel
+{
+    public class ExelMethods
+    {
+        const string path = "WeatherArchives/moskva_2010.xlsx";
+        const int columns = 12;
+
+        public string GetCellData(int rowNum, int cellNum)
+        {
+            IWorkbook workbook;
+            using (FileStream fileStream = new FileStream(path, FileMode.Open, FileAccess.Read))
+            {
+                workbook = new XSSFWorkbook(fileStream);
+            }
+
+            // Получение листа
+            ISheet sheet = workbook.GetSheetAt(0);
+            // Чтение данных из ячейки
+            IRow row = sheet.GetRow(rowNum); // строка
+            string cell = row.GetCell(cellNum)?.ToString(); // ячейка (столбец)
+            return cell;
+        }
+        public List<string> GetRowData(int rowNum)
+        {
+            var rowElems = new List<string>();
+            for (int i = 0; i < columns; i++)
+            {
+                rowElems.Add(GetCellData(rowNum, i));
+            }
+            return rowElems;
+        }
+        public string GetArchiveHeader()
+        {
+            return GetCellData(0, 0);
+        }
+        public string GetArchiveDescription()
+        {
+            return GetCellData(1, 0);
+        }
+        public string GetColumnName(int columnNum)
+        {
+            return GetCellData(columnNum, 0);
+        }
+        public List<string> GetColumnsNames()
+        {
+            var columnNames = new List<string>();
+            for (int i = 0; i < columns;i++)
+            {
+                var columnName = GetCellData(2, i) + " " + GetCellData(3, i);
+                columnNames.Add(columnName);
+            }
+            return columnNames;
+        }
+    }
+}
