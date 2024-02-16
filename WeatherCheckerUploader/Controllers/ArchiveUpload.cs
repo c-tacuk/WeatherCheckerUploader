90825587fce9a41ada9df99d7c3ebe18afff1d02
Стас Сухомлинов <DlyaSteam1997@gmail.com>
2024-02-16 14:10:18 +0300

    Сделал кнопку для загрузки файла. Написал метод для получения и сохранения файла в папку

--- WeatherCheckerUploader/Controllers/ArchiveUpload.cs
+++ WeatherCheckerUploader/Controllers/ArchiveUpload.cs
@@ -1,12 +1,32 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WeatherCheckerUploader.Models;
 
 namespace WeatherCheckerUploader.Controllers
 {
     public class ArchiveUpload : Controller
     {
+        IWebHostEnvironment _appEnvironment;
+        public ArchiveUpload(IWebHostEnvironment appEnvironment)
+        {
+            _appEnvironment = appEnvironment;
+        }
         public IActionResult Index()
         {
             return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> AddFile(IFormFile uploadedFile)
+        {
+            if (uploadedFile != null)
+            {
+                string path = "/WeatherArchives/" + uploadedFile.FileName; // путь к папке WeatherArchives
+                using (var fileStream = new FileStream(_appEnvironment.ContentRootPath + path, FileMode.Create)) // сохраняем файл в папку WeatherArchives
+                {
+                    await uploadedFile.CopyToAsync(fileStream);
+                }
+                var file = new WeatherArchiveModel { Name = uploadedFile.FileName };
+            }
+            return RedirectToAction("Index");
         }
     }
 }
